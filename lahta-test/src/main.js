b5a6767423889afdc799ee7f12ad18ce08269623
konst <konstantin.mikheev@icord.cz>
2017-10-01 10:26:53 +0200

    back changes

--- lahta-test/src/main.js
+++ lahta-test/src/main.js
@@ -7,25 +7,9 @@ import router from './router';
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
-const counter = 0;
 new Vue({
   el: '#app',
   router,
-  template: `
-    <div>
-      <p>{{counter}}</p>
-      <button v-on:click="incrementCounter">++</button>
-    </div>
-    `,
-  data() {
-    return {
-      counter,
-    };
-  },
-  methods: {
-    incrementCounter() {
-      this.counter += 1;
-    },
-  },
+  template: '<App/>',
   components: { App },
 });
