4a79c582f75258a8926379e7a71a1031ed383350
konst <konstantin.mikheev@icord.cz>
2017-10-01 15:03:38 +0200

    nested routes

---
+++ lahta-test/src/components/pages/admin/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="admin-new">
+      <div class="container">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <ul class="admin-menu">
+            <!-- Links are to sibling routes -->
+            <li><router-link to="/admin">View Products</router-link></li>
+            <li><router-link to="/admin/new">New Product</router-link></li>
+          </ul>
+        </div>
+        <!-- Outlet for children routes -->
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
--- lahta-test/src/router/index.js
+++ lahta-test/src/router/index.js
@@ -2,7 +2,11 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/pages/Home';
 import Cart from '@/components/pages/Cart';
-import Admin from '@/components/pages/Admin';
+
+import Index from '@/components/pages/admin/Index';
+import New from '@/components/pages/admin/New';
+import Products from '@/components/pages/admin/Products';
+import Edit from '@/components/pages/admin/Edit';
 
 Vue.use(Router);
 
@@ -14,14 +18,34 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/admin',
-      name: 'Admin',
-      component: Admin,
-    },
-    {
       path: '/cart',
       name: 'Cart',
       component: Cart,
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      // Parent routes still has a component
+      component: Index,
+
+      // Child routes
+      children: [
+        {
+          path: 'new',
+          name: 'New',
+          component: New,
+        },
+        {
+          path: '',
+          name: 'Products',
+          component: Products,
+        },
+        {
+          path: 'edit/:id',
+          name: 'Edit',
+          component: Edit,
+        },
+      ],
     },
   ],
 });
