b7c2a011dac91b44f5e689417dd63cbb0e76f316
konst <konstantin.mikheev@icord.cz>
2017-10-14 20:02:05 +0200

    webpack

--- lahta-test/build/webpack.base.conf.js
+++ lahta-test/build/webpack.base.conf.js
@@ -55,6 +55,10 @@ module.exports = {
         }
       },
       {
+        test: /\.styl$/,
+        loader: ['style-loader', 'css-loader', 'stylus-loader']
+      },
+      {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
