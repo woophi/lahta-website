a3ad6adee8c65ae5ed97e089935e3d53ee429bc7
konst <konstantin.mikheev@icord.cz>
2017-10-14 20:01:42 +0200

    eslint rules

--- lahta-test/.eslintrc.js
+++ lahta-test/.eslintrc.js
@@ -34,6 +34,19 @@ module.exports = {
       'optionalDependencies': ['test/unit/index.js']
     }],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // set maximum line characters
+    'max-len': [2, 140, 4, {'ignoreUrls': true, 'ignoreTemplateLiterals': true, 'ignoreStrings': true}],
+    'no-return-assign': 0,
+    // disallow indentation using both tabs and spaces
+    'no-mixed-spaces-and-tabs': 2,
+    // ensure consistent 2 space indentation and indent cases under switch
+    'indent': [2, 2, {'SwitchCase': 1}],
+    'object-curly-spacing': [2, 'always'],
+    'max-statements': [2, 24]
   }
 }
