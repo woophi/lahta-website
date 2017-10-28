eaf271c85ea85d6d43301d4376ae09ff3d1991d0
konst <konstantin.mikheev@icord.cz>
2017-10-28 21:54:24 +0200

    added map

--- lahta-test/src/components/pages/Contacts.vue
+++ lahta-test/src/components/pages/Contacts.vue
@@ -1,24 +1,63 @@
 <template>
-  <div>
-    <div class="title">
-      <h1>{{msg}}</h1>
+  <div class="contacts-container">
+    <OnlineForm class="contacts-form"></OnlineForm>
+    <div class="contatcs-info">
+      <div class="address">
+        193091, Санкт-Петербург, Октябрьская наб., д. 6, лит. М, пом. 10-Н,
+      </div>
+      <div class="tel-mob">
+        тел/факс +7 (812) 240-21-42
+      </div>
+      <div class="mail">
+        mail@mail.ru
+      </div>
     </div>
-    <div>map</div>
-    <div>
-      193091, Санкт-Петербург, Октябрьская наб., д. 6, лит. М, пом. 10-Н,
-      ИНН 7811598377 КПП 781101001
-      тел/факс +7 (812) 240-21-42
-    </div>
+    <yandex-map
+      :coords="[59.912021, 30.422444]"
+      zoom="13"
+      class="contacts-map"
+      :zoom-control="zoomControl"
+      :controls="['trafficControl']"
+      map-type="map"
+    >
+      <ymap-marker
+        marker-type="placemark"
+        :coords="[59.912021, 30.422444]"
+        hint-content="ООО «ЛАХТА ИНЖИНИРИНГ СПБ»"
+        :balloon="{header: 'header', body: 'body', footer: 'footer'}"
+        :icon="{color: 'green', glyph: 'dot'}"
+        cluster-name="1"
+      ></ymap-marker>
+    </yandex-map>
   </div>
 </template>
 
 <script>
+import OnlineForm from '@/components/pages/OnlineForm';
+import { yandexMap, ymapMarker } from 'vue-yandex-maps';
+
 export default {
   name: 'contacts',
   data() {
     return {
-      msg: 'Welcome to the Contacts Page',
+      zoomControl: {},
     };
+  },
+  components: {
+    OnlineForm,
+    yandexMap,
+    ymapMarker,
   },
 };
 </script>
+
+<style lang="stylus">
+.contacts-container
+  display flex
+  flex-wrap wrap
+
+.contatcs-info, contacts-form, .contacts-map
+  width 30%
+.contacts-map
+  height 500px
+</style>
